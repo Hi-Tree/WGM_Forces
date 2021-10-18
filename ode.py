3484c70e242430ce58e204a58869b38b4b6b09bb
Meri Khurshudyan <71793253+Hi-Tree@users.noreply.github.com>
2021-10-18 14:10:19 -0400

    Update ode.py

--- ode.py
+++ ode.py
@@ -6,51 +6,34 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 ################  Changes Made ##############
+#dtheta = v_x*np.cos(theta)*np.cos(phi)+v_y*np.cos(theta)*np.sin(phi)-v_z*np.sin(theta) 
+# #dphiv_y*np.cos(phi)-v_x*np.sin(phi)
 # randomize Rp mean = 750nm with 5% deviation 
 # adjust eta such that v * gamma = || 0.00700175 = v * 6*np.pi*eta*Rp
 # uniform random positions
 # purely data collective no graphs display, quits uppon reaching resonator 
 # ****** IMP if pos1 > pos2 and not reached resonator quit
-N = 1
-Results = pd.DataFrame()
-def randomized(vx_max):
-    rand_x = []
-    rand_y = []
-    rand_z = []
-    while len(rand_x) != N:
-        vx = maxwell.rvs()
-        vy = maxwell.rvs()
-        vz = maxwell.rvs()
-        if vx < vx_max: #less than max and positive
-            rand_x.append(0.01)
-            rand_y.append(0.01)
-            rand_z.append(0.01)
-    rand_v = np.zeros((N,3),dtype = float)
-    rand_v[:,0], rand_v[:,1], rand_v[:,2] = [rand_x,rand_y,rand_z]
-    return rand_v 
+# Manually Found that if dRho/dt > 0.0075 particle will not attach 
+j = 0
+TC = []
+###################################################    RANDOMIZE ALL VARIABLES #####################################################
+N = 5
+Results = pd.DataFrame(columns=['rand_x','rand_y','rand_z','rho_','theta_','phi_','dRho_','dTheta_','dPhi_','Rp_','gamma_','time_'])
+while Results.count()[0] != N:
+    Rp = np.random.uniform(7.5*10**(-7)-3.75*10**(-8), 7.5*10**(-7)+3.75*10**(-8))
+    rho = np.random.uniform(39.107,39.107+(2*np.pi))
+    theta = np.random.uniform((np.pi-np.sqrt(1/40)),(np.pi+np.sqrt(1/40)))
+    phi = np.random.uniform(0,np.pi*2)
+    vx = maxwell.rvs()
+    vy = maxwell.rvs()
+    vz = maxwell.rvs()
+    eta = (0.00700175)/(vx*6*np.pi*Rp)
+    gamma = 6*np.pi*eta*Rp 
+    dRhodT = vx*np.sin(theta)*np.cos(phi)+vy*np.sin(theta)*np.sin(phi)+vz*np.cos(theta)
+    if dRhodT <= 0.0075:
+        Results = pd.concat([pd.DataFrame([[vx,vy,vz,rho,theta,phi,dRhodT,0,0,Rp,gamma,None]],columns = Results.columns),Results],ignore_index = True)
 
-#Initial Positions
-rho = 39.107*(2 * np.pi) * 1.1 #because of deffinitions x_r is manually entered 
-theta = np.pi / 2
-phi = 0
-
-#Initial Velocities 
-values = randomized(.5)        
-v_x = values[:,0]
-v_y = values[:,1]
-v_z = values[:,2] 
-drho = v_x*np.sin(theta)*np.cos(phi)+v_y*np.sin(theta)*np.sin(phi)+v_z*np.cos(theta)
-dtheta = v_x*np.cos(theta)*np.cos(phi)+v_y*np.cos(theta)*np.sin(phi)-v_z*np.sin(theta)
-dphi = v_y*np.cos(phi)-v_x*np.sin(phi)
-
-Rp = np.random.normal(loc = 7.5*10**(-7), scale = 3.75*10**(-8), size = N)
-eta = [(0.00700175)/(v_x[i]*6*np.pi*Rp[i]) for i in range(N)]
-gamma_ = [6*np.pi*eta[i]*Rp[i] for i in range(N)]
-
-#v = sphere 750nm rad
-allData = []
-time = np.linspace(0, 80, 100)
-
+############################################################  Function #######################################################################################
 def f(u, t, par, gamma):
     rho, drho, theta, dtheta, phi, dphi = u
     dudt = [
@@ -58,21 +41,33 @@ def f(u, t, par, gamma):
             dtheta, (forces.theta(rho, theta, par) - gamma * rho * dtheta * np.cos(phi) - 2 * drho * dtheta * np.cos(phi) + 2 * rho * dtheta * dphi * np.sin(phi)) / (rho * np.cos(phi)),
             dphi, (forces.phi(rho, theta, par) - gamma * rho * dphi - 2 * drho * dphi - rho * dphi ** 2 * np.sin(phi) * np.cos(phi)) / rho
         ]
+    if rho <= 39.107:
+        TC.append(t)
     return dudt
+###################################################### Parameters and Solutions #######################################################################
 
-#Solve
-for j in range(N):
-    gamma = gamma_[j]
+time = np.linspace(0, 80, 100)
+while j != N:
+    gamma_ = Results['gamma_'][j]
     par = {
         'l': 40,
         'x_r': 39.107,            
-        'R_ratio': (Rp[j])/(5.6*10**(-5)), 
+        'R_ratio': (Results['Rp_'][j])/(5.6*10**(-5)), 
         'n_w': 1.326,
         'n_p': 1.572
         }
-    u0 = [rho, drho[j], theta, dtheta[j], phi, dphi[j]] 
+    u0 = [Results['rho_'][j],Results['dRho_'][j], Results['theta_'][j], Results['dTheta_'][j], Results['phi_'][j], Results['dPhi_'][j]] 
     sol = odeint(f, u0, time, args = (par, gamma*0))
-    
+    Results.at[j,'time_'] = TC[0]
+    TC.clear()
+    j+=1
+
+####################################################### Viewing Options ########################################################
+pd.set_option('display.max_colwidth', None)
+print(Results)
+#### End of Data Collection
+
+#v = sphere 750nm rad 
 #Graph
 '''
 fig = plt.figure()
