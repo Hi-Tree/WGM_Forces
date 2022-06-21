39776fa1a6838f7c404ad3b10f1f23f8ab9c8163
Meri Khurshudyan <71793253+Hi-Tree@users.noreply.github.com>
2022-06-21 08:20:27 -0400

    Update test.py

--- test.py
+++ test.py
@@ -4,29 +4,31 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
 
-time = np.linspace(0, 80, 100)
+time = np.linspace(0, 100, 100)
 
-def f(u, t, par, gamma):
+def f(u, t, par, gamma): #correct gamma tilde subs ****
     rho, drho, theta, dtheta, phi, dphi = u
     dudt = [
             drho, forces.rho(rho, theta, par) - gamma * drho + rho * (dtheta * np.cos(phi)) ** 2 + rho * dphi ** 2,
             dtheta, (forces.theta(rho, theta, par) - gamma * rho * dtheta * np.cos(phi) - 2 * drho * dtheta * np.cos(phi) + 2 * rho * dtheta * dphi * np.sin(phi)) / (rho * np.cos(phi)),
             dphi, (forces.phi(rho, theta, par) - gamma * rho * dphi - 2 * drho * dphi - rho * dphi ** 2 * np.sin(phi) * np.cos(phi)) / rho
         ]
+    if rho <= 39.107:
+        print("we did it")
     return dudt
 
-rho = 40
+rho = 41
 theta = np.pi/2
 phi = 0
 
-drho = 0.0075
-dtheta = 0.02
+drho = 0
+dtheta = 0
 dphi = 0
 # find how they are related 
 par = {
         'l': 40,
         'x_r': 39.107,            
-        'R_ratio': 7.5e-2 / 5.6, 
+        'R_ratio': (7.125e-2 + 3.75e-3)/ 5.6, 
         'n_w': 1.326,
         'n_p': 1.572
         }
